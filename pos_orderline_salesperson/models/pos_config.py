8953d5f89a0c3f0e1322f6431e0944efd99ad8ce
desarrolladores ingenioso <developer@ingenioso.co>
2026-07-27 19:17:59 -0500

    eliminacion espacio

--- pos_orderline_salesperson/models/pos_config.py
+++ pos_orderline_salesperson/models/pos_config.py
@@ -7,7 +7,7 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     allow_orderline_user = fields.Boolean(
-        string='Allow Orderline Salesperson', 
+        string='Allow Orderline Salesperson',
         help='Allow custom salesperson on Orderlines'
     )
     employee_salesperson_ids = fields.Many2many(
